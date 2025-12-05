3d0b1e175af8e0067d247141291552a896227b2f
lghuy05 <luonggiahuy1909@gmail.com>
2025-12-04 21:02:18 -0500

    implemented coordinate conversion

--- crime_predict_visualization/src/pages/Demo.tsx
+++ crime_predict_visualization/src/pages/Demo.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Rectangle, FeatureGroup, Tooltip } from 'react-leaflet';
 
+const createGridBoundfromCenter = (center: number, gridSizemeter: number): [[number, number], [number, number]] => {
+  const [centerLat, centerLong] = center;
+  const metersPerLat = 111111;
+  const metersPerLong = 111111 * Math.cos(centerLat * Math.PI / 180);
+  const halfGridLat = gridSizemeter / 2 / metersPerLat;
+  const halfGridLong = gridSizemeter / 2 / metersPerLong;
+  const southwest: [number, number] = [
+    centerLat - halfGridLat, centerLong - halfGridLong
+  ];
+  const northeast: [number, number] = [
+    centerLat + halfGridLat,
+    centerLong + halfGridLong
+  ];
+
+  return [southwest, northeast];
+}
 export default function ResearchGridMap() {
   const sarasotaCenter = [27.3364, -82.5307] as [number, number];
+  const rectangle_coordinate = createGridBoundfromCenter(sarasotaCenter, 500);
+  // const rectangle_coordinate = [[27.32, -82.55], [27.36, -82.51]];
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 p-4">
@@ -20,7 +38,12 @@ export default function ResearchGridMap() {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <FeatureGroup pathOptions={{ color: 'purple' }}>
+              <Rectangle bounds={rectangle_coordinate} />
+              <Tooltip>Crime</Tooltip>
+            </FeatureGroup>
           </MapContainer>
+
         </div>
       </div>
     </div>
